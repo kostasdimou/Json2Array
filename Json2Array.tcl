970ec0ee61f8331e0e427de88f110b670552e69f
Kostas Dimou <kostas.dimou@gmail.com>
2025-05-27 19:59:07 +0300

    Work in progress

--- Json2Array.tcl
+++ Json2Array.tcl
@@ -1,37 +1,66 @@
-proc parse {json path index jsonArrayName} {
-    upvar $jsonArrayName JSON
-    set name {}
-    set value {}
-    while {$json != {}} {
-        set next [string index $json 0]
-        if {$next == "\{"} {
-            incr index
-            set json [string range $json [expr 1 + [parse [string range $json 1 end]]] end]
-        } elseif {$next == "\""} {
-            if {$name == {}} {
-                set end [string first "\"" $json 1]
-                if {$end == -1} {
-                    return [string length $json]
-                }
-                set name [string range $json 1 [expr $end - 1]]
-                set json [string range $json [expr $end + 1] end]
-            } elseif {$value == {}} {
-                set end [string first "\"" $json 1]
-                if {$end == -1} {
-                    return [string length $json]
-                }
-                set value [string range $json 1 [expr $end - 1]]
-                JSON([join [list $path $name] "."]) $value
-                set json [string range $json [expr $end + 1] end]
-            }
+#!/usr/bin/tclsh
+proc json_parse {json jsonArrayName {start 0} {path {}}} {
+    array set ASCII {
+        blanks {20 9 10 13}
+        opening {123 91 40 34}
+        closing {125 93 41 34}
+        quotes 34
+        colon 58
+        openBrace 123
+        openBracket 91
+        closeBrace 125
+        closeBracket 93
+        openParenthesis 40
+        closeParenthesis 41
+    }
+    array set STATE {
+        previousState {
+            foundAscii nextState
+        }
+        idle {
+            openBrace opening
+        }
+        opening {
+            quotes name
+            comma opening
+        }
+        name {
+            colon value
+        }
+        value {
+            quotes opening
         }
     }
-}
+    set state idle
+    upvar $jsonArrayName JSON
+    set block {}
+    set match {}
+    set name {}
+    set value {}
+    set max [string length $json]
+    for {set i $start} {$i < $max} {incr i} {
+        set chr [string index $json $i]
+        set ascii [scan $chr %c]
+        if {$ascii in $ASCII(blanks) && $match != $ASCII(quotes)} continue
+        if {$ascii == $match} {
+            set block [lreplace $block end end]
+            set match [lindex $ASCII(closing) [lsearch $ASCII(opening) [lindex $block end]]]
+        } elseif {$ascii in $ASCII(opening)} {
+            lappend block $ascii
+            set match [lindex $ASCII(closing) [lsearch $ASCII(opening) $ascii]]
+        } else {
+            puts "$state"
+        }
+# puts "chr = $chr - ascii = $ascii - block = $block - match = $match"
+    }
+} ;# proc json_parse {}
 
-array set JSON {}
-set filename "sample.json"
-set in [open $filename r]
-set size [file size $filename]
-set json [read $in $size]
-parse $json {} -1 JSON
-parray JSON
+foreach filename $argv {
+    set in [open $filename r]
+    set size [file size $filename]
+    set json [read $in $size]
+    array unset JSON
+    array set JSON {}
+    json_parse $json JSON
+    parray JSON
+}
