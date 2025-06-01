37df55aea8df5c7299ccfac3de9688d902ded782
Kostas Dimou <kostas.dimou@gmail.com>
2025-06-01 12:12:46 +0300

    Update Json2Array.tcl
    State machine phase 1

--- Json2Array.tcl
+++ Json2Array.tcl
@@ -1,66 +1,108 @@
 #!/usr/bin/tclsh
-proc json_parse {json jsonArrayName {start 0} {path {}}} {
-    array set ASCII {
-        blanks {20 9 10 13}
-        opening {123 91 40 34}
-        closing {125 93 41 34}
-        quotes 34
-        colon 58
-        openBrace 123
-        openBracket 91
-        closeBrace 125
-        closeBracket 93
-        openParenthesis 40
-        closeParenthesis 41
+namespace eval json {
+  variable quotes 34
+  variable colon 58
+  variable openBrace 123
+  variable openBracket 91
+  variable closeBrace 125
+  variable closeBracket 93
+  variable openParenthesis 40
+  variable closeParenthesis 41
+  variable escape 92
+  variable space 20
+  variable tab 9
+  variable newLine 10
+  variable carriageReturn 13
+  variable dot 14
+
+  proc blanks {state} {
+    puts "proc blanks {state = $state}"
+    variable space
+    variable tab
+    variable newLine
+    variable carriageReturn
+    set nextState {}
+    foreach blank [list $space $tab $newLine $carriageReturn] {
+      lappend nextState [list $blank $nextState]
     }
-    array set STATE {
-        previousState {
-            foundAscii nextState
-        }
-        idle {
-            openBrace opening
-        }
-        opening {
-            quotes name
-            comma opening
-        }
-        name {
-            colon value
-        }
-        value {
-            quotes opening
-        }
-    }
-    set state idle
-    upvar $jsonArrayName JSON
-    set block {}
-    set match {}
-    set name {}
-    set value {}
-    set max [string length $json]
-    for {set i $start} {$i < $max} {incr i} {
-        set chr [string index $json $i]
-        set ascii [scan $chr %c]
-        if {$ascii in $ASCII(blanks) && $match != $ASCII(quotes)} continue
-        if {$ascii == $match} {
-            set block [lreplace $block end end]
-            set match [lindex $ASCII(closing) [lsearch $ASCII(opening) [lindex $block end]]]
-        } elseif {$ascii in $ASCII(opening)} {
-            lappend block $ascii
-            set match [lindex $ASCII(closing) [lsearch $ASCII(opening) $ascii]]
-        } else {
-            puts "$state"
-        }
-# puts "chr = $chr - ascii = $ascii - block = $block - match = $match"
-    }
-} ;# proc json_parse {}
+    return $nextState
+  }
+    
+  # STATE(previousState) {ascii1 nextState1 ascii2 nextState2}
+  array set STATE [list\
+    idle [list\
+      [list $openBrace open]\
+      [blanks idle]\
+    ]\
+    open [list\
+      [list $quotes nameRead]\
+      [blanks open]\
+    ]\
+    nameRead [list\
+      [list $quotes nameDone]\
+    ]\
+    nameDone [list\
+      [list $colon pair]\
+      [blanks nameDone]\
+    ]\
+    pair [list\
+      [list $quotes valueRead]\
+      [blanks pair]\
+    ]\
+    valueRead [list\
+      [list $quotes valueDone]\
+    ]\
+    valueDone [list\
+      [list $closeBrace idle]\
+      [blanks valueDone]\
+    ]\
+  ]
+  
+  proc parse {json jsonArrayName {start 0} {path {}}} {
+    puts "proc parse {json = $json, jsonArrayName = $jsonArrayName, start = $start, path = $path}"
+    variable STATE
+    variable dot
+      upvar $jsonArrayName JSON
+      set state idle
+      set name {}
+      set value {}
+      set max [string length $json]
+      for {set i $start} {$i < $max} {incr i} {
+          set char [string index $json $i]
+          set ascii [scan char %c]
+          puts "1 char = char - ascii = $ascii - path = path - state = $state"
+          array unset ASCII
+          array set ASCII $STATE($state)
+          set previousState $state
+          foreach ascii [array names ASCII]
+            if {$char == ascii {
+              set state $ASCII($ascii)
+              break
+            }
+          }
+          if {$state != $previousState} {
+            if {$state == nameRead} {
+              append name $char
+            }
+            if {$state == valueRead} {
+              append value $char
+            }
+          } else {
+            if {$state == valueDone} {
+              set JSON([join [list $path $name] $dot])
+            }
+          }
+          puts "2 path = path - state = $state"
+      }
+  } ;# proc json_parse {}
+}
 
 foreach filename $argv {
-    set in [open $filename r]
-    set size [file size $filename]
-    set json [read $in $size]
+    # set in [open $filename r]
+    # set size [file size $filename]
+    # set json [read $in $size]
     array unset JSON
     array set JSON {}
-    json_parse $json JSON
+    json::parse "{ "name": "John Doe" }" JSON
     parray JSON
 }
